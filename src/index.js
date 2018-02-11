5fe91e33459e4dcb5a1f0d99821fb6911c528956
卓文理 <531840344@qq.com>
2018-02-11 16:03:24 +0800

    :sparkles: 添加编辑框

--- src/index.js
+++ src/index.js
@@ -1,139 +1,175 @@
 /*
  * @Author: 卓文理
  * @Email: 531840344@qq.com
- * @Date: 2018-02-07 16:33:23
+ * @Date: 2018-02-08 14:37:19
  */
 
 'use strict';
 
-// import angular from 'angular';
-import Quill from 'quill';
-import Delta from 'quill-delta';
-import MarkBlot from './plugin/mark';
+import module from './module';
+import smsEditor from './editor';
 
-import './index.sass';
+const template = `
+<section class="ng-sms-editor">
+    <header class="ng-sms-editor__header">
+        <span class="text">插入</span>
+        <a href="javascript:;"
+            ng-repeat="label in ctrl.labels"
+            ng-click="ctrl.insert(label)"
+            class="label label-{{label.type}}"
+        >{{label.name}}</a>
+        <div class="sms-custom" ng-transclude="header"></div>
+    </header>
+    <sms-editor ng-model="ctrl.content" insert-text="ctrl.insertText"></sms-editor>
+    <footer class="ng-sms-editor__footer">
+        <div class="sms-custom" ng-transclude="footer"></div>
+        <span class="sms-total">已输入 <em>{{ctrl.contentLength}}</em> 个字，共计 <em>{{ctrl.countSms()}}</em> 条短信</span>
+        <span class="sms-rule">
+            计费规则
+            <div class="sms-tooltip">
+                <i class="sms-tooltip-arrow"></i>
+                <div class="sms-tooltip-inner">
+                    <p>1、70字计1条短信收费，超出70字按67字每条计。</p>
+                    <p>2、一个汉字，字母或标点都算一个字。</p>
+                    <p>3、自动替换的内容以实际发送为准，有可能超过默认字数。</p>
+                    <p>4、受短信运营商限制，签名和退订回T，包含在字数中</p>
+                </div>
+            </div>
+        </span>
+    </footer>
+</section>
+`;
 
-Quill.register(MarkBlot);
+class ngSmsEditor {
+    constructor($scope, $element, $sce) {
+        this.$scope = $scope;
+        this.$element = $element;
+        this.$sce = $sce;
 
-export default angular.module('ng-sms-editor', [])
-    .directive('ngSmsEditor', () => ({
-        restrict: 'AE',
-        require: 'ngModel',
-        scope: {
-            insertText: '=',
-            key: '<',
-        },
-        link($scope, element, attr, ngModel) {
-            if (!ngModel) return;
+        this.insertText = '';
+        this.content = this.content || '';
+        this.labels = this.labels || [];
+        this.placeholderMap = this.genPlaceholderMap(this.labels);
 
-            const options = Object.assign($scope.options || {}, {
-                modules: {
-                    toolbar: '',
-                    clipboard: {
-                        matchVisual: false
-                    }
-                },
-                formats: ['mark']
-            });
-
-            if (attr.placeholder != null) {
-                options.placeholder = attr.placeholder;
+        $scope.$watch('ctrl.labels', (newValue) => {
+            if (newValue.length) {
+                this.placeholderMap = this.genPlaceholderMap(this.labels);
             }
+        }, true);
+        $scope.$watch('ctrl.content', () => this.countContentLen());
+    }
 
-            const quill = new Quill(element[0], options);
+    /**
+     * 插入预设标签
+     * @param {Object} label 标签
+     */
+    insert(label) {
+        if (label.name === '#店铺名称#') {
+            this.insertText = label.placeholder;
+        } else if (label.type === 'placeholder') {
+            this.insertText = label.name;
+        }
+    }
 
-            // 填充文本
-            ngModel.$render = () => {
-                const value = ngModel.$viewValue;
+    /**
+     * 统计短信条数
+     * @return {number} 短信条数
+     */
+    countSms() {
+        const smsLen = this.contentLength;
+        let smsCount = 1;
 
-                if (value) {
-                    const textLength = quill.getLength();
+        if (smsLen > 70) {
+            smsCount = Math.ceil(smsLen / 67);
+        }
 
-                    if (textLength) {
-                        quill.deleteText(0, textLength);
-                    }
+        this.smsCount = smsCount;
+        return smsCount;
+    }
 
-                    value.replace(/#[^a-z0-9A-Z]+?#/g, text => `@@@${text}@@@`)
-                        .split('@@@')
-                        .filter(x => x)
-                        .map((text, inx) => {
-                            const length = quill.getLength();
+    /**
+     * 替换占位符为对应示例文案
+     * @param  {string} content 短信内容
+     * @return {string}         替换占位符后的内容
+     */
+    replaceLabels(content) {
+        const rreEscape = /[-/\\^$*+?.()|[\]{}]/g;
 
-                            if (/#[^a-z0-9A-Z]+#/.test(text)) {
-                                const id = parseInt(Math.random() * 1e8, 10);
-                                quill.insertEmbed(length - 1, 'mark', { value: text, id }, Quill.sources.USER);
-                                quill.insertText(length, ' ');
+        let reg;
 
-                                if (inx === value.length - 1) {
-                                    quill.insertText(length + 1, '\uFEFF');
-                                }
-                            } else {
-                                quill.insertText(length - 1, text);
-                            }
+        for (const key in this.placeholderMap) {
+            reg = new RegExp(key.replace(rreEscape, '\\$&'), 'g');
+            content = content.replace(reg, `<em>${this.placeholderMap[key]}</em>`);
+        }
 
-                            return text;
-                        });
-                }
-            };
+        return content;
+    }
 
-            // 监听文本插入
-            $scope.$watch('insertText', () => {
-                let value = $scope.insertText;
+    /**
+     * 生成占位符map
+     * @param  {array} labels  标签列表
+     * @return {object}        占位符map
+     */
+    genPlaceholderMap(labels) {
+        let label;
+        const map = {};
 
-                if (value) {
-                    const range = quill.getSelection(true);
-                    const id = parseInt(Math.random() * 1e8, 10);
+        for (let i = 0, len = labels.length; i < len; i += 1) {
+            label = labels[i];
 
-                    if (/c\.tb\.cn/.test(value)) {
-                        value = ` ${value} `;
-                    }
+            if (label.type === 'placeholder') {
+                map[label.name] = label.placeholder;
+            }
+        }
 
-                    quill.insertEmbed(range.index, 'mark', { value, id }, Quill.sources.USER);
+        return map;
+    }
 
-                    const length = quill.getLength();
+    /**
+     * 计算短信长度、生成预览短信
+     */
+    countContentLen() {
+        let contentParsered;
+        let contentLength;
+        let realContent;
+        const { content } = this;
 
-                    let text = ' ';
+        if (angular.isDefined(content)) {
+            contentParsered = this.replaceLabels(content);
+            realContent = contentParsered.replace(/<\/?em>/g, '');
+            contentLength = realContent.length;
 
-                    if (length - range.index <= 2) {
-                        text += '\uFEFF';
-                    }
+            // 计算长度之后替换内容
+            realContent = realContent.replace(/张三三/g, '#买家姓名#');
+            realContent = realContent.replace(/ c.tb.cn\/C.cxKC /g, '#付款链接#');
+            realContent = realContent.replace(/ c.tb.cn\/F.yyyyx /g, '#评价链接#');
 
-                    // magic code: 插入后加一个空格
-                    quill.insertText(range.index + 1, text);
-                    quill.setSelection(range.index + 2, Quill.sources.SILENT);
+            this.contentParsered = this.$sce.trustAsHtml(contentParsered);
+            this.realContent = realContent;
+            // 7 = 2('【】') + 5(' 退订回T');
 
-                    $scope.insertText = '';
-                }
-            });
+            this.contentLength = this.signContent.length + contentLength + 7;
+        }
+    }
+}
 
-            // 监听文本改动
-            quill.on('text-change', () => {
-                const { ops } = quill.getContents();
-                let text = '';
+ngSmsEditor.$inject = ['$scope', '$element', '$sce'];
 
-                ops.map((item) => {
-                    if (item.insert) {
-                        text += typeof item.insert === 'object' ? item.insert.mark : item.insert.trim();
-                    }
-                    return item;
-                });
-
-                // 移除非法字符、首尾空格
-                text = text.replace(/(&#65279;|\uFEFF)/g, '').trim();
-
-                ngModel.$setViewValue(text);
-            });
-
-            const emoji = /\ud83c[\udf00-\udfff]|\ud83d[\udc00-\ude4f]|\ud83d[\ude80-\udeff]|\ud83e[\udd10-\udd80]/g;
-
-            // 过滤剪贴板emoji字符
-            quill.clipboard.addMatcher(
-                Node.ELEMENT_NODE,
-                node => new Delta().insert(node.innerText.trim().replace(emoji, ''))
-            );
-            quill.clipboard.addMatcher(
-                Node.TEXT_NODE,
-                node => new Delta().insert(node.data.trim().replace(emoji, ''))
-            );
+export default module
+    .directive('smsEditor', smsEditor)
+    .component('ngSmsEditor', {
+        template,
+        controller: ngSmsEditor,
+        controllerAs: 'ctrl',
+        bindToController: true,
+        bindings: {
+            content: '=',
+            labels: '<',
+            signContent: '='
         },
-    }));
+        transclude: {
+            header: '?slotHeader',
+            footer: '?slotFooter'
+        },
+    });
+
---
+++ src/module.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 卓文理
+ * @Email: 531840344@qq.com
+ * @Date: 2018-02-08 14:36:12
+ */
+
+'use strict';
+
+export default angular.module('ng-sms-editor', []);
